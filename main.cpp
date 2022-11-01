0c06615e3315a38cd6410423b27d183fa4f24045
AKIRA_ENDO_0208 <k021g1166@g.neec.ac.jp>
2022-11-01 12:56:41 +0900

    制作途中

--- main.cpp
+++ main.cpp
@@ -1,43 +1,17 @@
 #include <stdio.h>
 
-//char以外のテンプレート
-template <typename Type>
-
-Type Min(Type a, Type b) {
-
-	if (a > b)
-	{
-		return static_cast<Type>(b);
+int Recursive(int hour) {
+	if (hour == 1) {
+		return (100);
 	}
 
-	if (a < b)
-	{
-		return static_cast<Type>(a);
-	}
-}
-
-//オーバーライド
-template<>
-char Min<char>(char a, char b) {
-	return printf("数字以外は代入できません");
+	return (hour * 2 - 50);
 }
 
 int main(void) {
+	int hour = 5;
+	int result;
 
-	//比較用引数
-	int a = 10;
-	int b = 13;
-	float c = 14.7f;
-	float d = 13.5f;
-	double e = 3.1415926;
-	double f = 1.9980409;
-	char g = 'a';
-	char h = 'b';
-
-
-	printf("%d\n", Min<int>(a,b));
-	printf("%f\n", Min<float>(c, d));
-	printf("%lf\n", Min<double>(e, f));
-	printf("%c\n", Min<char>(g, h));
+	result = Recursive(hour);
 
 }
