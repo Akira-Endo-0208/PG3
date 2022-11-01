e19e9c4c1407f7a3ef1e8274feb4a3d6cb54f6cd
AKIRA_ENDO_0208 <k021g1166@g.neec.ac.jp>
2022-11-01 12:25:06 +0900

    途中

---
+++ main.cpp
@@ -0,0 +1,62 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+using namespace std;
+
+
+typedef struct cell
+{
+	int val;
+	struct cell* next;
+}CELL;
+
+void create(CELL* head, int val);	//データを追加する関数のプロトタイプ宣言　
+void index(CELL* head);	//一覧を表示する関数のプロトタイプ宣言
+
+int main()
+{
+	int val;
+
+	CELL head;
+	head.next = nullptr;
+
+	while (true)
+	{
+		printf("数字を入力してください\n");
+		scanf_s("%d", &val);
+
+		//最後尾にセルを追加
+		create(&head, val);
+
+		printf("一覧\n");
+		//リスト一覧の表示
+		index(&head);
+	}
+
+	return 0;
+}
+
+void create(CELL* head, int val)
+{
+	CELL* beta;
+	//新規作成するセル分のメモリを確保する
+	beta = (CELL*)malloc(sizeof(CELL));
+
+	beta->val = val;
+	beta->next = nullptr;
+
+	//最後(最新)のセルのアドレスの1つめの処理は引数から持ってきた
+	//リストのうち最初にセルのアドレスが該当する
+	while (head->next != nullptr) {
+		head = head->next;
+	}
+	head->next = beta;
+}
+
+void index(CELL* head) {
+	while (head->next != nullptr) {
+		head = head->next;
+		printf("%d\n", head->val);
+	}
+}
