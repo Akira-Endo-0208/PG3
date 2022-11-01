5ea761bdbcd40d81e9fe6a0fe6bdd891ea5c655b
AKIRA_ENDO_0208 <k021g1166@g.neec.ac.jp>
2022-11-01 12:25:06 +0900

    feat:STLとコンテナクラスを学ぶため、山手線の駅名をリストで表示するプログラムを追加

---
+++ main.cpp
@@ -0,0 +1,72 @@
+#include<stdio.h>
+#include<list>
+#include<iostream>
+using namespace std;
+int main() {
+
+	//ƒRƒ“ƒeƒi–¼
+	list<const char*> stations =
+	{
+	"1970s","Shinagawa","Tamachi","Hamamatsucho","Shinbashi",
+	"Yurakucho","Tokyo","kanda","Akihabara","Okatimachi",
+	"Ueno","Uguisudani","Nippori","Tabata","Magome","Sugamo",
+	"Ootsuka","Ikebukuro","Mejiro","Takatababa","Shinjuku",
+	"Yoyogi","Harajuku","Shibuya","Ebisu","Meguro","Gotanda","Oosaki"
+	};
+
+	for (auto itr = stations.begin(); itr != stations.end(); ++itr)
+	{
+		cout << *itr << "\n";
+
+	}
+	printf("--------------------\n");
+
+	for (list<const char*>::iterator itr = stations.begin(); itr != stations.end(); ++itr)
+	{
+		if (strcmp(*itr,"1970s")== 0)
+		{
+
+			*itr = "2019s";
+			++itr;
+		}
+		if (strcmp(*itr,"Tabata") == 0)
+		{
+
+			itr = stations.insert(itr, "NishiNippori");
+			++itr;
+		}
+
+	}
+
+	for (auto itr = stations.begin(); itr != stations.end(); ++itr)
+	{
+		cout << *itr << "\n";
+	}
+
+	printf("--------------------\n");
+
+	for (list<const char*>::iterator itr = stations.begin(); itr != stations.end(); ++itr)
+	{
+		if (strcmp(*itr,"2019s")== 0)
+		{
+
+			*itr = "2022s";
+			++itr;
+		}
+		if (strcmp(*itr,"Tamachi") == 0)
+		{
+
+			itr = stations.insert(itr, "TakanawaGateway");
+			++itr;
+		}
+
+	}
+
+	for (auto itr = stations.begin(); itr != stations.end(); ++itr)
+	{
+		cout << *itr << "\n";
+	}
+
+	printf("--------------------\n");
+	return 0;
+}
